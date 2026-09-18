986bcb362db348b01d738234b872695ff1f1313c
cristianmorenor <cristian.moreno-r@mail.escuelaing.edu.co>
2026-09-17 23:27:46 -0500

    test: RED - spare suma 10 mas el primer tiro del siguiente frame

--- src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
+++ src/test/java/edu/eci/dosw/bowling/BowlingScorerTest.java
@@ -35,4 +35,22 @@ class BowlingScorerTest {
         // Act & Assert
         assertEquals(70, game.score());
     }
+
+    @Test
+    @DisplayName("Spare en frame 1 suma 10 mas el primer tiro del frame 2")
+    void spareInFirstFrame_addsBonusFromNextRoll() {
+        // Arrange
+        BowlingGame game = new BowlingGame();
+        game.roll(5);
+        game.roll(5); // spare frame 1
+        game.roll(3);
+        game.roll(0); // frame 2
+        for (int i = 0; i < 8; i++) {
+            game.roll(0);
+            game.roll(0);
+        }
+
+        // Act & Assert
+        assertEquals(16, game.score());
+    }
 }
