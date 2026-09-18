b2ca8c3b1fd7d2e0dc50edba6246717430b732c1
cristianmorenor <cristian.moreno-r@mail.escuelaing.edu.co>
2026-09-17 22:08:12 -0500

    chore: agregar esqueleto de BowlingGame y Frame

---
+++ src/main/java/edu/eci/dosw/bowling/Frame.java
@@ -0,0 +1,8 @@
+package edu.eci.dosw.bowling;
+
+/**
+ * Representa un frame del juego con sus tiros.
+ * Se completará a medida que la implementación (TDD) lo requiera.
+ */
+public class Frame {
+}
